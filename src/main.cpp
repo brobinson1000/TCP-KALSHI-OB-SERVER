568d08528a530450f2cd9f67e40b11faa12b0ced
Brandon Robinson <1802brad@gmail.com>
2026-01-23 10:55:02 -0500

    fix ob color

--- src/main.cpp
+++ src/main.cpp
@@ -58,13 +58,16 @@ void print_orderbook(const std::vector<Order> &orderbook) {
     for (const auto &order : orderbook) {
 
         if (order.active == true) {
-            std::cout << green <<" | " << order.orderid << "\t" << " | " << order.trader_id << "\t" << " | " <<
-            order.price << "\t" << " | " << order.prob_basis_point << "\t" << " | " << order.side << "\t" << " | " <<
-            std::boolalpha << order.active << "\n" << reset;
+            std::cout <<" | " << green << order.orderid << reset << "\t" << " | " << green << order.trader_id << reset
+            << "\t" << " | " << green <<
+            order.price << reset << "\t" << " | " << green << order.prob_basis_point << reset << "\t" << " | " << green
+            << order.side << reset << "\t" << " | " <<
+            std::boolalpha << green << order.active << "\n" << reset;
         } else {
-            std::cout << red << " | " << order.orderid << "\t" << " | " << order.trader_id << "\t" << " | " <<
-            order.price << "\t"<< " | " << order.prob_basis_point << "\t" << " | " << order.side << "\t" << " | " <<
-            std::boolalpha << order.active << "\n" << reset;
+            std::cout << " | " << red << order.orderid << reset << "\t" << " | " << red << order.trader_id << reset << "\t" << " | " <<
+            red << order.price << reset << "\t"<< " | " << red << order.prob_basis_point << reset << "\t" << " | " <<
+            red << order.side << reset << "\t" << " | " <<
+            std::boolalpha << red << order.active << "\n" << reset;
         }
     }
 };
